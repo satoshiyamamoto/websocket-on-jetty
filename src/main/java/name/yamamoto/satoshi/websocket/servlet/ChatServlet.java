7b1a0e214c85de92205bbfc3f0a4d35ef414c542
Satoshi Yamamoto <satoshi@yamamoto.name>
2014-08-19 16:36:43 +0900

    Add heroku executable files

--- src/main/java/name/yamamoto/satoshi/websocket/servlet/ChatServlet.java
+++ src/main/java/name/yamamoto/satoshi/websocket/servlet/ChatServlet.java
@@ -1,10 +1,13 @@
 package name.yamamoto.satoshi.websocket.servlet;
 
+import javax.servlet.annotation.WebServlet;
+
 import name.yamamoto.satoshi.websocket.handler.ChatHandler;
 
 import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
+@WebServlet(name="chat", urlPatterns={"/chat"})
 public class ChatServlet extends WebSocketServlet {
 	private static final long serialVersionUID = 1L;
 
